c385229d4deea68f85222fd280426067b986d3cf
Dmytro Bulhakov <mail@dbulgakov.com>
2025-04-30 13:36:25 +0300

    Up

--- infrastructure/config.py
+++
@@ -1,6 +0,0 @@
-import pulumi
-from pulumi import Config
-
-azure_config = Config("azure")
-location = azure_config.get("location") or "EastUS"
-resource_group_name = azure_config.get("resourceGroupName") or "agro-monitor-rg"
--- infrastructure/frontend.py
+++
@@ -1,7 +0,0 @@
-import pulumi
-
-def export_frontend_url(storage_account):
-    pulumi.export(
-        "frontendUrl",
-        storage_account.primary_endpoints.web
-    )
