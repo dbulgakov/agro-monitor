98195d16d748a25bba6795c54104a8d366bb324a
Dmytro Bulhakov <mail@dbulgakov.com>
2025-05-01 23:09:22 +0300

    Up

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -100,7 +100,7 @@ export default function HomePage() {
   // State for Date Picker
   const [startDate, setStartDate] = useState<Date | null>(() => {
       const date = new Date();
-      date.setMonth(date.getMonth() - 3);
+      date.setFullYear(date.getFullYear() - 2);
       return date;
   });
   const [endDate, setEndDate] = useState<Date | null>(new Date());
